83185e3bb08dafbac7e3fec3dfcb6336db2ed921
hcc95 <hcc95@qq.com>
2016-10-01 16:51:17 +0800

    update

--- src/jdbc/test/testDao.java
+++ src/jdbc/test/testDao.java
@@ -1,12 +1,16 @@
 package jdbc.test;
 import java.sql.SQLException;
+
 import org.junit.Test;
+
 import com.cdut.Dao.DAO;
+import com.cdut.bean.User;
 public class testDao {
 		@Test
 		public void testGet() throws SQLException {
 			String sql="SELECT id Id,loginName 用户名  FROM user WHERE id=?";
-
+			User user=DAO.get(User.class,sql,90);
+			System.out.println(user);
 		}
 	}
 
